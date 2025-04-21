9cbaf89b9f8e3874149118b0ca3767a8f53393d8
MITESH PATEL <mitesh64837@gmail.com>
2025-04-21 12:08:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from './components/home'
+import Home from './components/Home'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Project from './components/Project'
